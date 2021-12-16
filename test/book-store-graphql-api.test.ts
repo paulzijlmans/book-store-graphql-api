3918701216ef5482b0cdaec604d1351892f90f5f
Paul Zijlmans <paul.zijlmans@devolksbank.nl>
2021-12-16 22:29:00 +0100

    Initial commit

---
+++ test/book-store-graphql-api.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as BookStoreGraphqlApi from '../lib/book-store-graphql-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/book-store-graphql-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BookStoreGraphqlApi.BookStoreGraphqlApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
